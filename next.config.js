0799b3702ffe84d26371df4ba3f427587d3fe5f9
LempekPL <superlempek2004@gmail.com>
2022-04-16 01:09:59 +0200

    future config update

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
     reactStrictMode: true,
+    images: {
+        domains: ["avatars.githubusercontent.com"]
+    }
 });
