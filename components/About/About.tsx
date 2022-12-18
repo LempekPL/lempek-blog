09b707a83d37d294da892b10be6da977fe6b322c
LempekPL <superlempek2004@gmail.com>
2022-12-18 19:47:19 +0100

    about lempek changes (removed useless quick data box)

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,15 +1,78 @@
 import Image from "next/image";
 import {
-    AboutMargins, AboutBox, LempekAbout, LempekImage, Name, LempekDesc, DescPointer
+    AboutMargins,
+    AboutBox,
+    LempekAbout,
+    LempekImage,
+    LempekNames,
+    Name,
+    Nick,
+    LempekDesc,
+    LinksList,
+    LinkText,
+    ButtonIcon,
+    LinksLink,
+    LempekDescBox,
+    LempekInformation
 } from "../../styles/About/about";
 import useTranslation from "next-translate/useTranslation";
-import type {AboutData} from "../../types/about";
-import Accounts from "./AboutLinks";
-import Contributors from "./AboutContributors";
+import type {Socials} from "../../types/about";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const About = ({data}: { data: AboutData }) => {
+const socials: Socials[] = [
+    {
+        name: "github",
+        link: "https://github.com/LempekPL",
+        color: "#171515"
+    },
+    {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/piotrlempek/",
+        color: "#0077B5"
+    },
+    {
+        name: "twitter",
+        link: "https://twitter.com/LEMPEKPL",
+        color: "#1DA1F2"
+    },
+    {
+        name: "discord",
+        link: "https://discord.com/channels/@me/249253855613812736",
+        color: "#5865F2"
+    },
+    {
+        name: "twitch",
+        link: "https://www.twitch.tv/lempekpl",
+        color: "#9146FF"
+    },
+    {
+        name: "youtube",
+        link: "https://www.youtube.com/@Lempek",
+        color: "#FF0000"
+    },
+    {
+        name: "steam",
+        link: "https://steamcommunity.com/id/LempekPL/",
+        color: "#0B4A7C"
+    }
+]
+
+const About = () => {
     const {t} = useTranslation("about");
-    const years = Math.abs(Math.round((Date.now() - Number(new Date(2004, 2, 29))) / (1000 * 60 * 60 * 24 * 365.25)));
+    const years = Math.floor((Date.now() - Number(new Date(2004, 2, 29))) / (1000 * 60 * 60 * 24 * 365.25));
+
+    const button_link_list = [];
+    for (let i = 0; i < socials.length; i++) {
+        button_link_list.push(
+            <LinksLink key={socials[i].name}
+                // @ts-ignore
+                       companyColor={socials[i].color}
+                       href={socials[i].link} target="_blank">
+                <ButtonIcon><FontAwesomeIcon icon={["fab", socials[i].name]}/></ButtonIcon>
+                <LinkText>{socials[i].name[0].toUpperCase() + socials[i].name.slice(1)}</LinkText>
+            </LinksLink>
+        );
+    }
 
     return <>
         <AboutMargins>
@@ -18,14 +81,21 @@ const About = ({data}: { data: AboutData }) => {
                     <LempekImage>
                         <Image src={"/images/lempekIRL.png"} alt="Lempek" layout="fill" priority={true}/>
                     </LempekImage>
-                    <Name>PIOTR LEMPKOWSKI</Name>
-                    <DescPointer/>
-                    <LempekDesc>
-                        {t("meIRL", {years})}
-                    </LempekDesc>
+                    <LempekNames>
+                        <Name>Piotr Lempkowski</Name>
+                        <Nick>LEMPEK</Nick>
+                    </LempekNames>
                 </LempekAbout>
-                <Accounts data={data}/>
-                <Contributors data={data}/>
+                <LempekInformation>
+                    <LempekDescBox>
+                        <LempekDesc>
+                            {t("meIRL", {years})}
+                        </LempekDesc>
+                    </LempekDescBox>
+                    <LinksList>
+                        {button_link_list}
+                    </LinksList>
+                </LempekInformation>
             </AboutBox>
         </AboutMargins>
     </>;
--- styles/About/about.ts
+++ styles/About/about.ts
@@ -9,17 +9,11 @@ export const AboutMargins = styled.div`
     1fr;
 
   @media (min-width: 1000px) {
-    grid-template-columns:
-            1fr
-            min(60rem, 100%)
-            1fr;
+    grid-template-columns: 1fr min(60rem, 100%) 1fr;
   }
 
-  @media (max-width: 600px) {
-    grid-template-columns:
-            0
-            100%
-            0;
+  @media (max-width: 700px) {
+    grid-template-columns: 0 100% 0;
   }
 `;
 
@@ -28,6 +22,7 @@ export const AboutBox = styled.div`
   grid-column: 2;
   align-items: center;
   background-color: #505050; // TODO: theme system
+  z-index: 1;
 `;
 
 export const LempekAbout = styled.div`
@@ -36,13 +31,11 @@ export const LempekAbout = styled.div`
   margin-left: var(--self-margin);
   margin-right: var(--self-margin);
   align-items: center;
-  display: grid;
-  grid-template-columns: 14rem 1fr;
-  grid-template-areas: "pic name" "pic meirl" "links links";
+  display: flex;
+  flex-direction: row;
   gap: 2rem;
 
-  @media (max-width: 600px) {
-    display: flex;
+  @media (max-width: 700px) {
     flex-direction: column;
     margin: calc(var(--self-margin) + var(--navbar-height)) auto auto;
     padding: 0;
@@ -51,10 +44,9 @@ export const LempekAbout = styled.div`
 `;
 
 export const LempekImage = styled.div`
-  grid-area: pic;
   height: 14rem;
-  position: relative;
   width: 14rem;
+  position: relative;
 
   img {
     border-radius: 100%;
@@ -63,16 +55,67 @@ export const LempekImage = styled.div`
   }
 `;
 
-export const Name = styled.div`
-  align-self: flex-end;
+export const LempekNames = styled.div`
+  align-self: center;
   color: ${props => props.theme.text};
+`;
+
+export const Name = styled.div`
   background-color: ${props => props.theme.background};
-  grid-area: name;
+  position: relative;
   font-size: 3rem;
   font-weight: bold;
   text-align: center;
-  padding: 1rem 2rem;
-  border-radius: 5rem;
+  padding: .5rem 1.5rem;
+  border-radius: 2rem 2rem 2rem 0;
+  width: fit-content;
+
+  ::before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+
+    background-color: #505050;
+    left: 14.45rem;
+    bottom: -4rem;
+    height: 4rem;
+    width: 2rem;
+    border-top-left-radius: 2rem;
+    box-shadow: 0 -2rem 0 0 ${props => props.theme.background};
+
+    @media (max-width: 999px) {
+      left: 12.35rem;
+    }
+
+    @media (max-width: 700px) {
+      left: 12.3rem;
+    }
+  }
+
+  @media (max-width: 999px) {
+    font-size: 2rem;
+    line-height: 2.5rem;
+  }
+
+  //@media (max-width: 600px) {
+  //  align-self: center;
+  //}
+  //
+  //@media (max-width: 350px) {
+  //  font-size: 12vw;
+  //  line-height: 16vw;
+  //  border-radius: 0;
+  //  padding: 0;
+  //}
+`;
+
+export const Nick = styled.div`
+  background-color: ${props => props.theme.background};
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-align: center;
+  padding: .5rem 2rem;
+  border-radius: 0 0 2rem 2rem;
   width: fit-content;
 
   @media (max-width: 999px) {
@@ -80,38 +123,87 @@ export const Name = styled.div`
     line-height: 2.5rem;
   }
 
-  @media (max-width: 600px) {
-    align-self: center;
-  }
-
-  @media (max-width: 350px) {
-    font-size: 12vw;
-    line-height: 16vw;
-    border-radius: 0;
-    padding: 0;
-  }
 `;
 
-export const LempekDesc = styled.span`
+export const LempekInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 3rem 2rem;
+`;
+
+export const LempekDescBox = styled.div`
   color: ${props => props.theme.text};
-  grid-area: meirl;
-  margin: 0 1rem;
-  z-index: 1;
+`;
+
+export const LempekDesc = styled.p`
   padding: 1rem;
   border-radius: 1rem;
   background-color: ${props => props.theme.background};
 `;
 
-export const DescPointer = styled.div`
-  grid-area: meirl;
+export const LinksList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: ${props => props.theme.background};
+`;
+
+export const LinksLink = styled.a`
+  overflow: hidden;
+  width: 12rem;
+  text-align: center;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-decoration: none;
   position: relative;
-  border: solid .75rem transparent;
-  margin-left: -.40rem;
-  width: 1rem;
-  transition: 300ms;
-  border-right-color: ${props => props.theme.background};
-  
-  @media (max-width: 600px) {
-    display: none;
+  border-radius: 100rem;
+  border: 2px solid ${props => // @ts-ignore
+          props.companyColor};
+  color: #eee;
+  background-color: #eee;
+
+  :focus-visible {
+    outline: royalblue solid 2px;
   }
+
+  :is(:focus-visible, :hover)::after {
+    transform: translateY(-100%);
+  }
+
+  :is(:focus-visible, :hover) span {
+    transition: .25s;
+    color: ${props => // @ts-ignore
+            props.companyColor};
+  }
+
+  ::after {
+    transition: .25s;
+    content: "";
+    background-color: ${props => // @ts-ignore
+            props.companyColor};
+    position: absolute;
+    transform: translateY(0);
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+  }
+`;
+
+export const LinkText = styled.span`
+  position: relative;
+  z-index: 1;
+`;
+
+export const ButtonIcon = styled.span`
+  margin-right: .5rem;
+  position: relative;
+  z-index: 1;
 `;
