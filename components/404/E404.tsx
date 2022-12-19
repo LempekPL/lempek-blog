fd7a03f3dce1ae3137d357f7abb2c752f2b7f6eb
LempekPL <superlempek2004@gmail.com>
2022-12-20 00:56:22 +0100

    add translation to 404

--- components/404/E404.tsx
+++ components/404/E404.tsx
@@ -2,12 +2,13 @@ import {E404Box, E404Margins, E404TextBox} from "../../styles/E404/e404";
 import useTranslation from "next-translate/useTranslation";
 
 const E404 = () => {
+    const {t} = useTranslation("common");
 
     return <>
         <E404Margins>
             <E404Box>
                 <E404TextBox>
-                    <p>404<br/>Not Found</p>
+                    <p>{t("error")} 404<br/>{t("notFound")}</p>
                 </E404TextBox>
             </E404Box>
         </E404Margins>
